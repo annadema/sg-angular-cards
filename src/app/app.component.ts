7472aead7d3f2dd79aba3a0194f22a50d447fca4
aladar <annamaria.demartino@gmail.com>
2023-06-17 11:33:36 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sg-angular-cards';
+  posts = [
+    {
+      title: 'Sugar plum',
+      imageUrl: 'assets/SugarPlumCookies.png',
+      username: 'byron',
+      content: 'Cupcake ipsum dolor sit amet ',
+    },
+    {
+      title: 'Gingerbread',
+      imageUrl: 'assets/cake.jpg',
+      username: 'jolyker',
+      content: 'Sesame snaps biscuit icing chocolate cheesecake.',
+    },
+    {
+      title: 'Cake pie danish',
+      imageUrl: 'assets/peach.jpg',
+      username: 'crusty',
+      content: 'Halvah jelly beans marzipan gummi',
+    },
+  ];
 }
